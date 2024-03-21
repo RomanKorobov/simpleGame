5699d49655b5d40d2ab5723ce79eb29f617a401a
Roman Korobov <randymarsh87@bk.ru>
2024-03-21 21:06:19 +0300

    New interface added

--- app/src/main/java/com/example/simplegame/PlayFragment.kt
+++ app/src/main/java/com/example/simplegame/PlayFragment.kt
@@ -1,13 +1,14 @@
 package com.example.simplegame
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.simplegame.databinding.FragmentPlayBinding
 
-class PlayFragment : Fragment(R.layout.fragment_play) {
+class PlayFragment : Fragment(R.layout.fragment_play), SwipeHandler {
     private lateinit var binding: FragmentPlayBinding
     private lateinit var swipeListener: SwipeListener
 
@@ -17,11 +18,27 @@ class PlayFragment : Fragment(R.layout.fragment_play) {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentPlayBinding.inflate(inflater, container, false)
-        swipeListener = SwipeListener(requireContext())
+        swipeListener = SwipeListener(requireContext(), this)
         binding.playContainer.setOnTouchListener(swipeListener)
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun swipeUp() {
+        Log.d("GEST", "up")
+    }
+
+    override fun swipeDown() {
+        Log.d("GEST", "down")
+    }
+
+    override fun swipeLeft() {
+        Log.d("GEST", "left")
+    }
+
+    override fun swipeRight() {
+        Log.d("GEST", "right")
     }
 }
